02ded4c04eb57b98623315d9fb76387fa469b315
ghazariann <45097291+ghazariann@users.noreply.github.com>
2024-06-12 13:04:56 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -33,7 +33,7 @@ EOF
     sudo systemctl start etcd
 fi
 # Go
-# export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:/usr/local/go/bin"
 if  go version &> /dev/null
 then
     echo "Go is installed"
